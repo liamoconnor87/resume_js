6f9a3cc3e754dd4d09b89b53b2d60efe7a78bf5b
liamoconnor87 <liamoconnor87@gmail.com>
2020-07-29 15:12:28 +0000

    githubinfo js code tidy

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -18,10 +18,12 @@ function userInformationHTML(user){
 
 function repoInformationHTML(repos) {
     if(repos.length == 0) {
+
         return `<div class = "clearfix repo-list">No repos!</div>`
     }
 
     var listItemHTML = repos.map(function(repo){
+        
         return `
             <li>
                 <a href = "${repo.html_url}" target = "_blank">${repo.name}</a>
@@ -44,35 +46,47 @@ function fetchGitHubInformation(event){
 
     $("#gh-user-data").html("");
     $("#gh-repo-data").html("");
-
     var username = $("#gh-username").val();
     if (!username) {
+
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return
     }
-
     $("#gh-user-data").html(
+
         `<div>
             <img src="assets/css/loader.gif" alt= "loading...">
         </div>`
     );
-
     $.when(
+
         $.getJSON(`https://api.github.com/users/${username}`),
         $.getJSON(`https://api.github.com/users/${username}/repos`)
-    ).then(
+    )
+    .then(
+
         function(firstResponse, secondResponse) {
+
             var userData = firstResponse[0];
             var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
             $("#gh-repo-data").html(repoInformationHTML(repoData));
-        }, function(errorResponse) {
+        }, 
+        function(errorResponse) {
 
             if (errorResponse.status === 404) {
+
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`
                 );
-            } else {
+            } 
+            else if (errorResponse.status === 403) {
+
+                var resetTime = new Date(errorResponse.getResponseHeader(`X-RateLimit-Reset`)*1000);
+                $("#gh-user-data").html(`<h4>Too many request, please wait untill ${resetTime.toLocaleTimeStriing()}</h4>`)
+            } 
+            else {
+
                 console.log(errorResponse);
                 $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`)
             }
