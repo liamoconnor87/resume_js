a2d019af8d7d5cf760999ea6482c59109dbaa7e6
liamoconnor87 <liamoconnor87@gmail.com>
2020-07-30 10:11:02 +0000

    email sending set up

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+function sendMail(contactForm) {
+    emailjs.send("gmail", "new_test", {
+        "from_name":contactForm.name.value,
+        "from_email":contactForm.emailaddress.value,
+        "project_request":contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAIL", error);
+        }
+    );
+}
